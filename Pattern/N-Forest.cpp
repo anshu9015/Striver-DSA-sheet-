837600029695b3baa98351389c56cd34b3f6abec
anshu9015 <anshu.kmr9015@gmail.com>
2023-11-29 04:50:43 +0530

    adding solutions

---
+++ Pattern/N-Forest.cpp
@@ -0,0 +1,19 @@
+void nForest(int n) {
+	  for(int i=0;i<n;++i){
+
+        for(int j=0;j<n;++j){
+
+            cout<<"* ";
+
+        }
+
+        cout<<"\n";
+
+    }
+}
+// The output should be like this in the below:
+// * * * * * 
+// * * * * * 
+// * * * * * 
+// * * * * * 
+// * * * * * 
