77d2348dae41b91dc29ad5ce4036594f42aabec0
anshu9015 <anshu.kmr9015@gmail.com>
2023-11-29 22:21:45 +0530

    nNumber triangle formed

---
+++ DSA/Pattern/N-numberTriangle.cpp
@@ -0,0 +1,15 @@
+void nNumberTriangle(int n) {
+    for(int i =n;i>0;i--){
+        for(int j = 0;j<i;++j){
+            cout<<j+1<<" ";
+        }
+        cout<<endl;
+    }
+    // Write your code here.
+}
+
+// 1 2 3 4 5 
+// 1 2 3 4 
+// 1 2 3 
+// 1 2 
+// 1 
