4d5600d1496364f09bb7399a76864661e648f245
anshu9015 <anshu.kmr9015@gmail.com>
2023-11-29 22:12:24 +0530

    seeding formed

---
+++ DSA/Pattern/seeding.cpp
@@ -0,0 +1,17 @@
+void seeding(int n) {
+	for(int i = n; i> 0; i--){
+		for(int j = i; j > 0;j--){
+			cout<<"* ";
+		}
+		cout<<endl;
+	}
+	// Write your code here.
+}
+
+
+
+// * * * * * 
+// * * * * 
+// * * * 
+// * * 
+// * 
