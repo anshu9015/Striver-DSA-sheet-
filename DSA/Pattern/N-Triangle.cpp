2e0b163b2d8a828724fe8a633d09b1eef8a2931f
anshu9015 <anshu.kmr9015@gmail.com>
2023-11-29 22:06:47 +0530

    ntriangle

---
+++ DSA/Pattern/N-Triangle.cpp
@@ -0,0 +1,16 @@
+void nTriangle(int n) {
+	for(int i = 0; i<n; ++i){
+		for(int j = 0; j<i+1; ++j){
+			cout<<j+1<<" ";
+		}
+		cout<<endl;
+	}
+	// Write your code here
+}
+
+
+// 1 
+// 1 2 
+// 1 2 3 
+// 1 2 3 4 
+// 1 2 3 4 5 
